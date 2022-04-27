0451572bc3a76bf066dbded9fbd3ea47e89cfe81
Tobias Wilker <twilker@phoenixcontact.com>
2022-04-27 14:42:12 +0200

    Revert approval. Lower max count.

--- game_contract/src/lib.rs
+++ game_contract/src/lib.rs
@@ -1,27 +1,13 @@
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
 use near_sdk::env::attached_deposit;
-use near_sdk::{env, near_bindgen, PanicOnDefault, ext_contract, AccountId, Gas, Promise, PromiseResult, require};
-use near_contract_standards::non_fungible_token::Token;
+use near_sdk::{env, near_bindgen, PanicOnDefault, ext_contract, AccountId, Gas, Promise};
 use std::str::FromStr;
 
 pub const MINT_GAS: Gas = Gas(30_000_000_000_000);
-pub const APPROVE_CALLBACK_GAS: Gas = Gas(10_000_000_000_000);
-pub const APPROVAL_GAS: Gas = Gas(20_000_000_000_000);
 
 #[ext_contract(ext_nft_mint)]
 trait MintContract {
     fn nft_mint(&self, token_owner_id: AccountId) -> Token;
-}
-
-#[ext_contract(ext_nft_approval)]
-trait NonFungibleTokenApprovalManagement: NonFungibleToken {
-    fn nft_approve(&mut self, token_id: String, account_id: String, msg: Option<String>);
-}
-
-//callbacks
-#[ext_contract(ext_self)]
-trait SelfContract {
-    fn start_approval(&self) -> Promise;
 }
 
 #[near_bindgen]
@@ -42,47 +28,24 @@ impl Contract {
         &mut self,
         count: u8
     ) {   
-        assert!(count <= 10, "You can buy a maximum of 10 animals at a time.");
+        assert!(count <= 8, "You can buy a maximum of 8 animals at a time because of the gas limits.");
         assert!(attached_deposit() >= (count as u128)*10u128.pow(24), "Each animal costs 1 NEAR. Please attach enough NEAR.");
         let mut promise: Option<Promise> = None;
         for _ in 0..count{
-            let next_promise = ext_nft_mint::nft_mint(
-                env::predecessor_account_id(), 
-                AccountId::from_str("kawaii-zoo-nft.cryptosketches.testnet").unwrap(), 
-                env::attached_deposit(),
-                MINT_GAS
-            ).then(
-                ext_self::start_approval(env::current_account_id(), 0, APPROVE_CALLBACK_GAS)
-            );
             match promise{
-                Some(old) => promise = Some(old.then(next_promise)),
-                None => promise = Some(next_promise)
-            }
-        }
-    }
-
-    pub fn start_approval(&self) -> Promise {
-        require!(env::predecessor_account_id() == env::current_account_id(), "Method is private");
-        assert_eq!(
-            env::promise_results_count(),
-            1,
-            "This is a callback method"
-        );
-
-        match env::promise_result(0) {
-            PromiseResult::NotReady => unreachable!(),
-            PromiseResult::Failed => panic!("Previous minting failed. Stopping loop."),
-            PromiseResult::Successful(result) => {
-                let minted_token = near_sdk::serde_json::from_slice::<Token>(&result).unwrap();
-                ext_nft_approval::nft_approve(
-                    minted_token.token_id, 
-                    env::current_account_id().to_string(), 
-                    Some("Auto approve game for NFT.".into()), 
+                Some(old) => promise = Some(old.then(ext_nft_mint::nft_mint(
+                    env::predecessor_account_id(), 
                     AccountId::from_str("kawaii-zoo-nft.cryptosketches.testnet").unwrap(), 
-                    0, 
-                    APPROVAL_GAS
-                )
-            },
+                    env::attached_deposit(),
+                    MINT_GAS
+                ))),
+                None => promise = Some(ext_nft_mint::nft_mint(
+                    env::predecessor_account_id(), 
+                    AccountId::from_str("kawaii-zoo-nft.cryptosketches.testnet").unwrap(), 
+                    env::attached_deposit(),
+                    MINT_GAS
+                ))
+            }
         }
     }
 }
