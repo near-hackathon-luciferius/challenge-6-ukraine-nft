25a57b53d6dcf7915d655948f7b671525e7ed09a
Tobias Wilker <twilker@phoenixcontact.com>
2022-04-27 07:35:43 +0200

    Add build script.

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+TARGET="${CARGO_TARGET_DIR:-target}"
+set -e
+cd "$(dirname $0)"
+
+cargo build --all --target wasm32-unknown-unknown --release
+cp $TARGET/wasm32-unknown-unknown/release/kawaii-zoo-nft.wasm ./res/
+cp $TARGET/wasm32-unknown-unknown/release/kawaii-zoo-game.wasm ./res/
